c8c3b80b0f5fa5712e05f33a74f83de1d0d68fb5
Nikhil Pevekar <nikhil.pevekar@thomsonreuters.com>
2024-12-11 12:02:13 +0530

    Code refactoring & intital commit for workflow dispatcher -Reduced unnecessary xmals. -Checked for hard coding.

---
+++ .codedworkflows/ConnectionsFactory.cs
@@ -0,0 +1,26 @@
+using UiPath.CodedWorkflows;
+using System;
+
+namespace TR_Aut_Dispatcher_2023
+{
+    public class ExcelFactory
+    {
+        public ExcelFactory(ICodedWorkflowsServiceContainer resolver)
+        {
+        }
+    }
+
+    public class O365MailFactory
+    {
+        public O365MailFactory(ICodedWorkflowsServiceContainer resolver)
+        {
+        }
+    }
+
+    public class OneDriveFactory
+    {
+        public OneDriveFactory(ICodedWorkflowsServiceContainer resolver)
+        {
+        }
+    }
+}
